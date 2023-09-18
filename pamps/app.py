2c97656ed0106a8526244e0cb19c76026862f00a
Flauberth <flauberth12@gmail.com>
2023-09-18 16:42:23 -0500

    feat: subir a imagem docker para o postgree e o app

---
+++ pamps/app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="Pamps",
+    version="0.1.0",
+    description="Pamps is a posting app"
+)
+
+@app.get("/")
+async def index():
+    return {"hello": "world"}
