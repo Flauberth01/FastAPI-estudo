c0521fcf10a122ea09222f4ff5dd56a69a8de44f
Flauberth <flauberth12@gmail.com>
2023-09-19 08:34:54 -0500

    feat: modelar models user e criar conexão ao BD

--- pamps/app.py
+++ pamps/app.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 app = FastAPI(
     title="Pamps",
     version="0.1.0",
-    description="Pamps is a posting app"
+    description="Pamps is a posting app clone to twitter"
 )
 
 @app.get("/")
